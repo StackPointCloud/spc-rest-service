bc90091a5ead16b3d73c42ba0f0c6cffe11d8f73
Matt Baldwin <baldwinmathew@gmail.com>
2014-05-05 21:27:56 -0700

    Changes

--- spc/urls.py
+++ spc/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
 from api import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
 
@@ -12,5 +13,9 @@ router.register(r'architecture', views.ArchitectureViewSet)
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
+    url(r'^architectures/$', views.ArchitectureList.as_view()),
+	url(r'^architectures/(?P<pk>[0-9]+)/$', views.ArchitectureDetail.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
